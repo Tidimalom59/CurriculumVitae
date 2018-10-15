8a21323be7e57e64addb420db9de40e217f47eb4
Tidimalo <mtidimalo59@gmail.com>
2018-10-15 06:34:12 +0200

    Add project files.

---
+++ CurriculumV/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CurriculumV.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
